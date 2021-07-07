f352f868a1a277308f22bfbbc665b4ee4789a400
Salar <salar.taeb@hotmail.com>
2021-07-07 00:01:13 -0700

    adding state for modal open/close

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import {Container} from './styles/navbar.js'
+import React, {useState} from 'react';
+import {Container} from './styles/navbar.js';
+import Sidebar from '../sidebar'
+import sidebarOverlayContext from '../sidebar/index.js'
+
 
 export default function Navbar () {
-  
+  const [modalVisible, setModalVisible] = useState('false');
+
+
   return (
     <Container>
+      <Sidebar
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
       <p>Hello</p>
-      <button></button>
+      <button onClick={() => setModalVisible(true)}>Sidebar</button>
     </Container>
   )
 };
